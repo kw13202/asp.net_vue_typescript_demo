ad28b3f7e77448836380ecddb91568c5b31a96fc
HarryWR <kw13202@163.com>
2018-08-03 14:14:08 +0800

    命令行配置demo

---
+++ 命令行配置/CommandLineSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;//引用了Microsoft.AspNetCore.All
+
+namespace CommandLineSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var setting = new Dictionary<string, string>
+            {
+                {"name", "Ben"},
+                {"age", "18"}
+            };
+
+            var builder = new ConfigurationBuilder()
+                .AddInMemoryCollection(setting)         //设置默认值
+                .AddCommandLine(args);                  //参数通过 dotnet xxx.dll name="JJJ" age="99"格式传入
+
+            var configuration = builder.Build();
+
+            Console.WriteLine($"name:{configuration["name"]}");
+            Console.WriteLine($"age:{configuration["age"]}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
