c6d014baec7818d00452ddcabb2bcb0ffb50992b
HarryWR <kw13202@163.com>
2018-08-21 16:51:47 +0800

    Bind读取配置到C#实例

---
+++ Bind读取配置到C#实例/OptionBindSample/Class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OptionBindSample
+{
+    public class Class
+    {
+        public int ClassNo { get; set; }
+        public string ClassDesc { get; set; }
+        public List<Student> Students { get; set; } = new List<Student>();
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
