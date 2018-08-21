4df5ef8ba6c757378d8ad743518fee5c90e7da85
HarryWR <kw13202@163.com>
2018-08-21 15:51:48 +0800

    Json文件配置

---
+++ Json文件配置/JsonConfigSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace JsonConfigSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("class.json");
+
+            var configuration = builder.Build();
+
+            Console.WriteLine($"ClassNo:{ configuration["ClassNo"] }");
+            Console.WriteLine($"ClassDesc:{ configuration["ClassDesc"] }");
+
+            Console.WriteLine("Students");
+
+            Console.Write(configuration["Students:0:Name"]);
+            Console.WriteLine(configuration["Students:0:Age"]);
+
+            Console.Write(configuration["Students:1:Name"]);
+            Console.WriteLine(configuration["Students:1:Age"]);
+
+            Console.ReadLine();
+        }
+    }
+}
