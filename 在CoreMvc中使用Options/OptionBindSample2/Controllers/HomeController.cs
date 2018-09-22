bffab012cc8cc4df846b946d8fab2c35cfae0574
HarryWR <kw13202@163.com>
2018-09-22 11:26:26 +0800

    在CoreMvc中使用Options

---
+++ 在CoreMvc中使用Options/OptionBindSample2/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace OptionBindSample2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Class _myClass;
+
+        public HomeController(IOptions<Class> classOption)
+        {
+            _myClass = classOption.Value;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_myClass);
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+    }
+}
